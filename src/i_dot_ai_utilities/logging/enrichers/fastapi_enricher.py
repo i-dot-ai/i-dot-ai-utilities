63a104c7a83ccf0f60fc0b169c5ad946d2abc85c
Ryan White <ryan.white@cabinetoffice.gov.uk>
2025-06-26 11:00:23 +0100

    add x-forwarded-for to fastapi context

--- src/i_dot_ai_utilities/logging/enrichers/fastapi_enricher.py
+++ src/i_dot_ai_utilities/logging/enrichers/fastapi_enricher.py
@@ -17,6 +17,9 @@ class FastApiEnricher:
                 "request_method": request.method,
                 "request_base_url": str(request.base_url),
                 "request_user_agent": request.headers.get("user-agent", "none"),
+                "request_x_forwarded_for": request.headers.get(
+                    "x-forwarded-for", "none"
+                ),
                 "request_path": request.url.path,
                 "request_query": request.url.query,
             }
