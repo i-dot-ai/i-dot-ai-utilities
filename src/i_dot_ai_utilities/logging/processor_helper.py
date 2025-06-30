91ddf21a63891ed83f63b025492742de31e6d2bb
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-06-30 13:30:15 +0100

    Ruff formatting

--- src/i_dot_ai_utilities/logging/processor_helper.py
+++ src/i_dot_ai_utilities/logging/processor_helper.py
@@ -9,9 +9,7 @@ from i_dot_ai_utilities.logging.types.log_output_format import LogOutputFormat
 
 class ProcessorHelper:
     def configure_processors(self, log_level: int, log_format: LogOutputFormat) -> None:
-        structlog.configure(
-            wrapper_class=structlog.make_filtering_bound_logger(log_level)
-        )
+        structlog.configure(wrapper_class=structlog.make_filtering_bound_logger(log_level))
         structlog.configure(
             processors=[
                 structlog.contextvars.merge_contextvars,
@@ -22,9 +20,7 @@ class ProcessorHelper:
             ]
         )
 
-    def _load_output_processors(
-        self, output_type: LogOutputFormat
-    ) -> tuple[Callable, ...]:
+    def _load_output_processors(self, output_type: LogOutputFormat) -> tuple[Callable, ...]:
         match output_type:
             case LogOutputFormat.JSON:
                 return (
