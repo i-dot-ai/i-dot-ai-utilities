e6250557395174c46b8397125675a88c078c5e7c
Ryan White <ryan.white@cabinetoffice.gov.uk>
2025-07-25 11:18:31 +0100

    add missing type for context enricher

--- src/i_dot_ai_utilities/logging/types/context_enrichment_options.py
+++ src/i_dot_ai_utilities/logging/types/context_enrichment_options.py
@@ -4,8 +4,9 @@ from i_dot_ai_utilities.logging.enrichers.enrichment_provider import (
     ContextEnrichmentType,
 )
 from i_dot_ai_utilities.logging.enrichers.fastapi_enricher import RequestLike
+from i_dot_ai_utilities.logging.types.lambda_enrichment_schema import LambdaContextLike
 
 
 class ContextEnrichmentOptions(TypedDict):
     type: ContextEnrichmentType
-    object: RequestLike
+    object: RequestLike | LambdaContextLike
