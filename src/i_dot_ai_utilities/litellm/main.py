2b82b2196560a08d9a0dbc975a8e56d5504a2251
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-07-27 16:45:32 +0100

    Correct function names and exception paths for litellm main.py

--- src/i_dot_ai_utilities/litellm/main.py
+++ src/i_dot_ai_utilities/litellm/main.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import litellm
 import requests
-from litellm import check_valid_key, completion, embedding
+from litellm import BadRequestError, check_valid_key, completion, embedding
 from litellm.litellm_core_utils.streaming_handler import CustomStreamWrapper
 from litellm.llms.openai.common_utils import OpenAIError
 from litellm.types.utils import EmbeddingResponse, ModelResponse
@@ -15,7 +15,7 @@ from i_dot_ai_utilities.logging.structured_logger import StructuredLogger
 settings = Settings()
 
 
-def _check_model_is_callable(model: str, logger: StructuredLogger | None = None) -> bool:
+def _check_chat_model_is_callable(model: str, logger: StructuredLogger | None = None) -> bool:
     result = check_valid_key(model, litellm.api_key)
     if not result and logger:
         logger.error("Model {model} is not available on key {api_key}", model=model, api_key=litellm.api_key[:6])
@@ -38,13 +38,15 @@ class LiteLLMHandler:
             litellm.api_version = settings.api_version
         if settings.organisation:
             litellm.organization = settings.organisation
+        self.chat_model = settings.chat_model
+        self.embedding_model = settings.embedding_model
         litellm.request_timeout = settings.timeout
 
-        response = check_valid_key(settings.model, litellm.api_key)
+        response = _check_chat_model_is_callable(self.chat_model, self.logger)
         if not response:
             #  Slicing API key to not expose entire key in logs
             self.logger.error(
-                "Invalid API key {api_key} for model {model}", api_key=litellm.api_key[:6], model=settings.model
+                "Invalid API key {api_key} for model {model}", api_key=litellm.api_key[:6], model=self.chat_model
             )
         if settings.langfuse_public_key and settings.langfuse_secret_key:
             self.logger.info("Langfuse callback configured by environment variables")
@@ -79,24 +81,28 @@ class LiteLLMHandler:
         :raises MiscellaneousLiteLLMException: occurs when the called method in the
         litellm sdk returns a generic openai exception
         """
-        if (model and not _check_model_is_callable(model)) and not _check_model_is_callable(settings.model):
-            raise ModelNotAvailableError("The given model is not available on this api key", 401)
         try:
+            if model and not _check_chat_model_is_callable(model, self.logger):
+                raise ModelNotAvailableError("The given model is not available on this api key", 401)
+            if not model and not _check_chat_model_is_callable(self.chat_model, self.logger):
+                raise ModelNotAvailableError("The default model is not available on this api key", 401)
+
             return completion(
-                model=model or settings.model,
+                model=model or self.chat_model,
                 messages=messages,
                 temperature=temperature or settings.temperature,
                 max_tokens=max_tokens or settings.max_tokens,
                 stream=should_stream,
                 **kwargs,
             )
+        except BadRequestError as e:
+            self.logger.exception("Failed to get chat completion")
+            raise MiscellaneousLiteLLMError(str(e), 400) from e
         except OpenAIError as e:
             self.logger.exception("Failed to get chat completion")
-            raise MiscellaneousLiteLLMError(
-                "Failed to get chat completion, generic error occurred inside LiteLLM", 500
-            ) from e
+            raise MiscellaneousLiteLLMError(str(e), 500) from e
 
-    async def get_embedding(self, text: str, model: str | None = None, **kwargs: dict[str, Any]) -> EmbeddingResponse:
+    def get_embedding(self, text: str, model: str | None = None, **kwargs: dict[str, Any]) -> EmbeddingResponse:
         """
         Async method for embedding given text
         :param text: The text to embed
@@ -107,20 +113,21 @@ class LiteLLMHandler:
         :raises MiscellaneousLiteLLMException: occurs when the called method in the
         litellm sdk returns a generic openai exception
         """
-        if (model and not _check_model_is_callable(model)) and not _check_model_is_callable(settings.model):
-            raise ModelNotAvailableError("The given model is not available on this api key", 401)
+
+        # LiteLLM doesn't support any way to pre-validate an embedding model
         try:
-            response: EmbeddingResponse = await embedding(model=model or settings.model, input=text, **kwargs)
+            response = embedding(model=model or self.embedding_model, input=text, **kwargs)
+        except BadRequestError as e:
+            self.logger.exception("Failed to get embedding")
+            raise MiscellaneousLiteLLMError(str(e), 400) from e
         except OpenAIError as e:
             self.logger.exception("Failed to get embedding")
-            raise MiscellaneousLiteLLMError(
-                "Failed to get embedding, generic error occurred inside LiteLLM", 500
-            ) from e
+            raise MiscellaneousLiteLLMError(str(e), 500) from e
         else:
             return response
 
     @staticmethod
-    def get_available_models() -> list[str]:
+    def get_all_models() -> list[str]:
         """
         Returns a list of available models
         :return: list[str] The models available
