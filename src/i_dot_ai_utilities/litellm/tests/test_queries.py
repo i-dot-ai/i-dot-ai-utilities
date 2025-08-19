26cf72902a7b6a47f5c9944c373422cad72951a4
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-08-19 15:21:49 +0100

    Fix tests

--- src/i_dot_ai_utilities/litellm/tests/test_queries.py
+++ src/i_dot_ai_utilities/litellm/tests/test_queries.py
@@ -1,6 +1,6 @@
 import pytest
-from litellm.litellm_core_utils.streaming_handler import CustomStreamWrapper
-from litellm.types.utils import EmbeddingResponse, ModelResponse
+from ecologits.tracers.litellm_tracer import ChatCompletion
+from litellm.types.utils import EmbeddingResponse
 
 from i_dot_ai_utilities.litellm.exceptions import MiscellaneousLiteLLMError, ModelNotAvailableError
 from i_dot_ai_utilities.litellm.main import settings
@@ -11,7 +11,7 @@ def test_chat(litellm_client):
         [{"role": "user", "content": "This is a test, please reply with just the word 'Hello' if you are available"}]
     )
     assert result
-    assert type(result) is ModelResponse
+    assert isinstance(result, ChatCompletion)
     assert result.choices[0].message.content == "Hello"
 
 
@@ -29,13 +29,18 @@ def test_invalid_model(litellm_client):
 
 
 def test_chat_stream(litellm_client):
-    result = litellm_client.chat_completion(
-        [{"role": "user", "content": "This is a test, please reply with just the word 'Hello' if you are available"}],
-        should_stream=True,
-    )
-    assert result
-    assert type(result) is CustomStreamWrapper
-    assert result.completion_stream.model_response.choices[0].message.content == "Hello"
+    response_content = ""
+    for chunk in litellm_client.chat_completion_stream(
+        messages=[
+            {"role": "user", "content": "This is a test, please reply with just the word 'Hello' if you are available"}
+        ],
+    ):
+        if hasattr(chunk, "choices") and chunk.choices:
+            delta = chunk.choices[0].delta
+            if delta["content"]:
+                content = delta["content"]
+                response_content += content
+    assert response_content == "Hello"
 
 
 def test_default_models_exists_in_model_list(litellm_client):
@@ -55,7 +60,7 @@ def test_get_embedding(litellm_client):
     result = litellm_client.get_embedding("This is a test text for embedding", model="text-embedding-3-small")
 
     assert result
-    assert type(result) is EmbeddingResponse
+    assert isinstance(result, EmbeddingResponse)
     assert result.data
     assert len(result.data) > 0
     assert result.data[0]["embedding"]
