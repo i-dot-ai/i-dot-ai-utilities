7a86bb766cc1bab45bf696507b5d517007d2061f
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-09-17 14:02:32 +0100

    Update exported module properties

--- src/i_dot_ai_utilities/file_store/__init__.py
+++ src/i_dot_ai_utilities/file_store/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ["factory"]
+from factory import create_file_store
+from main import FileStore
+
+from .types.file_store_destination_enum import FileStoreDestinationEnum
+
+__all__ = ["FileStore", "FileStoreDestinationEnum", "create_file_store"]
